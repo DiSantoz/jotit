9a7243d6833c1855e53ffc519e513edcbb63c61f
David Ferreira <dferreira@MacBook-Pro.fios-router.home>
2021-06-27 12:44:08 -0400

    corrected error in which notes did not properly display when added or deleted

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const express = require('express');
 const uniqid = require('uniqid');
 const app = express();
 const PORT = process.env.PORT || 3001;
-const data = require('./db/db.json');
-const { NOTFOUND } = require("dns");
 
 // parse incoming string or array data(middleware)
 app.use(express.urlencoded({ extended: true }));
@@ -15,16 +13,17 @@ app.use(express.json());
 // file path to public folder(middleware)
 app.use(express.static('public'));
 
-// GET and POST Routes
+// GET,POST, and DELETE Routes
 
-// Route for notes HTML
+// Route to display notes HTML
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-// Route for db.json file
+// Route to display notes array in db.JSON
 app.get('/api/notes', (req, res) => {
-  return res.json(data);
+  const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'))
+  return res.json(notes);
 });
 
 // Post route to save new note and add to db
@@ -44,8 +43,10 @@ app.post('/api/notes', (req, res) => {
 // Delete note 
 app.delete('/api/notes/:id', (req, res) => {
   const noteId = (req.params.id).toString();
+  // read current notes
   const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'))
   console.log(notes);
+  // remove notes based on ID
   const removeId = notes.filter(notes => notes.id !== noteId);
   fs.writeFileSync('./db/db.json', JSON.stringify(removeId, null, 2))
   res.json(notes);
